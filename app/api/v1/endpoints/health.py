0f959acfd675a85c28cb5fb0ecf13c95a8d66ce2
Edgar G Vargas Ocampo <egiovanni.vo@gmail.com>
2024-06-27 11:25:24 -0600

    feat: :hammer: Response Health Model

--- app/api/v1/endpoints/health.py
+++ app/api/v1/endpoints/health.py
@@ -1,11 +1,22 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class HealthResponseModel(BaseModel):
+    status: str
 
-@router.get("/", tags=["health"])
+
+@router.get(
+    "/",
+    tags=["health"],
+    summary="Health Check",
+    description="Check the health status of the API",
+    response_description="Health status of the API",
+    response_model=HealthResponseModel,
+)
 async def get_health_status():
     """
     Endpoint to check the health status of the API
     """
-    return {"status": "Ok"}
+    return HealthResponseModel(status="ok")
